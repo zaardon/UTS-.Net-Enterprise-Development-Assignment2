870c2709ef1c43a7dc9ac906656055f4054bfb8e
James-Chahwan <j.r.chahwan@gmail.com>
2014-05-11 22:34:06 +1000

    sort of got Consultant/Index.cshtml working

---
+++ BlueConsultingManagementSystem/BlueConsultingManagementSystem/Controllers/ConsultantController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlueConsultingManagementSystem.Controllers
+{
+    public class ConsultantController : Controller
+    {
+        //
+        // GET: /Consultant/
+        public ActionResult Index()
+        {
+            showExpenses();
+            return View();
+        }
+        public ActionResult CreateNew()
+        {
+            new Models.ConsultantModel().TogetherAdd("replace with real values", 13.37, "Rep it");
+            ViewBag.Title = "shiiiet";
+            return View();
+        }
+        public void showExpenses()
+        {
+            using (var db = new BCMSModelContainer())
+            {
+                foreach (var xp in db.Expenses)
+                {
+                    Response.Write(xp.Description + " " + xp.Amount);
+                }
+
+            }
+        }
+	}
+}
